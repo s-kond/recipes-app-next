563ed14236cdcf3336e3135037724cb764c99ab9
Stefan Kondring <kondring.s@gmx.de>
2022-10-11 16:39:47 +0200

    challenge3 - image

--- pages/Veggie.js
+++ pages/Veggie.js
@@ -1,6 +1,11 @@
+import Image from "next/image"
+import pizzaBackground from "../public/pizza-background.jpg"
+
+
 export default function Veggie(){
     return (
         <>
+            <Image src={pizzaBackground} alt="Sliced veggie pizza" layout="responsive"/>
             <h2>Veggie</h2>
             <p>May the animals live long and prosper.</p>
         </>
