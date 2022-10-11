79a95ab208d427454102633e95dd32ea690422d2
Stefan Kondring <kondring.s@gmx.de>
2022-10-11 16:25:04 +0200

    initial commit

---
+++ pages/Dessert.js
@@ -0,0 +1,8 @@
+export default function Dessert(){
+    return (
+        <>
+            <h1>Desserts</h1>
+            <p>Sweet...</p>
+        </>
+    )
+}
---
+++ pages/Veggie.js
@@ -0,0 +1,8 @@
+export default function Veggie(){
+    return (
+        <>
+            <h1>Veggie</h1>
+            <p>May the animals live long and prosper.</p>
+        </>
+    )
+}
