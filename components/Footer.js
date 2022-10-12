ec4a66b1228919bd53c292a80e0688647c685ae6
Stefan Kondring <kondring.s@gmx.de>
2022-10-12 12:05:29 +0200

    implement styled-components

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,17 @@
+import styled from "styled-components";
+
 export default function Footer() {
-    return (
-        <div className="footer">
-            <p >Made by Stefan</p>
-        </div>
-    )
+  return (
+    <FooterContainer>
+      <p>Made by Stefan</p>
+    </FooterContainer>
+  );
 }
+
+const FooterContainer = styled.div`
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  padding: 20px;
+  border-top: 2px dotted grey;
+`;
--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,14 @@
-export default function Header(){
-    return (
-        <div className="header">
-        <h1>Recipes App</h1>
-        </div>
-    )
+import styled from "styled-components";
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <h1>Recipes App</h1>
+    </HeaderContainer>
+  );
 }
+
+const HeaderContainer = styled.div`
+  width: 100%;
+  border-bottom: 2px dotted grey;
+`;
